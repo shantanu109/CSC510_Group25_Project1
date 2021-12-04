adcb6109ab279046335d3ebf8f8e50483300e82c
Urmi Pathak <53376580+urmi6899@users.noreply.github.com>
2021-12-04 18:20:06 -0500

    Create Home.test.js

---
+++ src/tests/Home.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { Provider } from 'react-redux'
+import Home from '../../components/Home';
+import { configureStore } from '../../store'
+
+const store = configureStore()
+
+test('createJob', () => {
+ const wrapper = shallow(<createJob/>);
+ expect(wrapper).toMatchSnapshot();
+});
+
+const search = {
+    results: []
+};
+
+const job = [
+        {_id: 1,
+        name: 'test',
+        status: '0',
+        location: 'test',
+        description: 'test',
+        pay: 'test',
+        schedule: 'test',
+        managerid: 'test',
+    }
+]
+
+test('render', () => {
+    const wrapper = mount(
+        <Provider store={store}>
+            <Home search={search} job = {job}/>
+        </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+
+});
