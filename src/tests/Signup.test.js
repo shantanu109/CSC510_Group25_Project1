2acbfc6b84add261f7d22d61c2c35882badb158e
Urmi Pathak <53376580+urmi6899@users.noreply.github.com>
2021-12-04 18:24:11 -0500

    Create Signup.test.js

---
+++ src/tests/Signup.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { Provider } from 'react-redux'
+import Signup from '../../components/Signup';
+import { configureStore } from '../../store'
+
+const store = configureStore()
+
+const error = {
+    
+};
+
+test('render', () => {
+    const wrapper = mount(
+        <Provider store={store}>
+            <Signup />
+        </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+
+});
