c927c062309b37013b7d92086beffd021e8afa69
Urmi Pathak <53376580+urmi6899@users.noreply.github.com>
2021-12-04 18:23:33 -0500

    Create Login.test.js

---
+++ src/tests/Login.test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { shallow ,mount} from 'enzyme';
+import { Provider } from 'react-redux'
+import Login from '../../components/Login';
+import {configureStore} from '../../store';
+
+const store = configureStore()
+
+
+
+const login = {
+    email:"test",
+    password:"test"
+}
+ 
+const location = {
+    state : "test"
+}
+
+test('login', () => {
+ const wrapper = shallow(
+    <Provider store={store}>
+    <Login login={login} />
+    </Provider>
+ );
+ expect(wrapper).toMatchSnapshot();
+
+ });
+test('render', () => {
+    const wrapper = mount(
+        <Provider store={store}>
+            <Login login={login} location = {location}/>
+        </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+});
