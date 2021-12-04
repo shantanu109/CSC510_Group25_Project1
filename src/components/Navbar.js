100afc683c3219b4c41998a5160562a7167b1e30
gargvarun250 <89566418+gargvarun250@users.noreply.github.com>
2021-12-04 14:58:12 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -102,7 +102,7 @@ class Navbar extends React.Component {
             
               <div className="header__option ">
                 <Link to="/goal">
-                  <WorkIcon fontSize="large" />
+                  <ListAltIcon fontSize="large" />
                 </Link>
               </div>
             
