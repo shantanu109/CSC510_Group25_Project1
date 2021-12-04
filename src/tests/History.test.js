6210f6b37f98f40cc9705fb625240f65db8d99a6
Urmi Pathak <53376580+urmi6899@users.noreply.github.com>
2021-12-04 18:18:03 -0500

    Update History.test.js

--- src/tests/History.test.js
+++ src/tests/History.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import TodoLists from '../../components/THistory';
+import TodoLists from '../../components/History';
 test('should test History component', () => {
  const wrapper = shallow(<History />);
  expect(wrapper).toMatchSnapshot();
