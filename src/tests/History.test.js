f8f7c3131ad473afeea7a2e057e596f999e2d477
Urmi Pathak <53376580+urmi6899@users.noreply.github.com>
2021-12-04 18:22:47 -0500

    Update History.test.js

--- src/tests/History.test.js
+++ src/tests/History.test.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import { shallow } from 'enzyme';
-import TodoLists from '../../components/History';
-test('should test History component', () => {
- const wrapper = shallow(<History />);
- expect(wrapper).toMatchSnapshot();
+import { shallow, mount } from 'enzyme';
+import { Provider } from 'react-redux'
+import History from '../../components/History';
+import { configureStore } from '../../store'
+
+const store = configureStore()
+
+const user = {
+    _id : "test"
+};
+
+const application = []
+
+test('render', () => {
+    const wrapper = mount(
+        <Provider store={store}>
+            <History user={user} application = {application}/>
+        </Provider>
+    );
+    expect(wrapper).toMatchSnapshot();
+
 });
