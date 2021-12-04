cb21d6152b3b93c86ca9661cebd8b2912b2a15c9
Urmi Pathak <53376580+urmi6899@users.noreply.github.com>
2021-12-04 18:25:08 -0500

    Create Page404.test.js

---
+++ src/tests/Page404.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Page404 from '../../components/Page404';
+
+test('error404', () => {
+ const wrapper = shallow(<Page404 />);
+ expect(wrapper).toMatchSnapshot();
+});
