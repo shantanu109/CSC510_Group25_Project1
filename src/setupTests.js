f4f9276c2f0a0be0c9f170de1cc363fd92eb28d8
Urmi Pathak <53376580+urmi6899@users.noreply.github.com>
2021-12-04 19:04:42 -0500

    Update setupTests.js

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,9 +1,6 @@
-// import { configure } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-// import 'jest-enzyme';
-// configure({ adapter: new Adapter() });
-
 import { configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-//import 'jest-enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import 'jest-enzyme';
 configure({ adapter: new Adapter() });
+
+
